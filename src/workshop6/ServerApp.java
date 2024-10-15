16c93e63a333206bae3a4012d4a015e0987d930c
kenken64 <bunnyppl@hotmail.com>
2024-10-15 14:51:04 +0800

    upd

--- src/workshop6/ServerApp.java
+++ src/workshop6/ServerApp.java
@@ -12,49 +12,64 @@ import static workshop6.Cookie.getRandomCookie;
 import static workshop6.Cookie.init;
 
 public class ServerApp {
-    public static final String ARG_MESSAGE="usage: java workshop6.ServerApp <port> <cookie file>";
+    public static final String ARG_MESSAGE = "usage: java workshop6.ServerApp <port> <cookie file>";
+
     public static void main(String[] args) throws NumberFormatException, IOException {
         System.out.println("ServerApp");
-        if(args.length < 2) {
+        if (args.length < 2) {
             System.out.println(ARG_MESSAGE);
             return;
         }
 
         String serverPort = args[0];
         String cookieFile = args[1];
-        System.out.println("ServerApp: serverPort=" 
-                    + serverPort + ", cookieFile=" + cookieFile);
-        
-        try (ServerSocket server = 
-                new ServerSocket(Integer.parseInt(serverPort))){
+        System.out.println("ServerApp: serverPort=" + serverPort + ", cookieFile=" + cookieFile);
+
+        try (ServerSocket server = new ServerSocket(Integer.parseInt(serverPort))) {
             init(cookieFile);
-            while(true){
-                try(Socket client = server.accept();
-                    InputStream is = client.getInputStream();
-                    DataInputStream dis = new DataInputStream(is);
-                    OutputStream os = client.getOutputStream();
-                    DataOutputStream dos = new DataOutputStream(os)){
-                    while(true){
-                        String commandMessage = dis.readUTF();
-                        System.out.println("ServerApp: message=" + commandMessage);
-                        if(commandMessage.equals("exit")){
-                            System.exit(1);
-                        }else if(commandMessage.equals("get-cookie")){
-                            // call the cookie object and randomize
-                            String randomCookie = "";
-                            try{
-                                randomCookie = getRandomCookie();
-                            } catch (NoCookieFoundException e) {
-                                System.err.println("ServerApp: NoCookieFoundException: " + e.getMessage());
-                            }
-                            
-                            dos.writeUTF("cookie-text_"+ randomCookie);
-                        }else{
-                            dos.writeUTF("Invalid command");
-                        }    
+            while (true) {
+                Socket client = server.accept();
+                ClientHandler clientHandler = new ClientHandler(client);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        }
+    }
+}
+
+class ClientHandler implements Runnable {
+    private final Socket client;
+
+    public ClientHandler(Socket client) {
+        this.client = client;
+    }
+
+    @Override
+    public void run() {
+        try (InputStream is = client.getInputStream();
+             DataInputStream dis = new DataInputStream(is);
+             OutputStream os = client.getOutputStream();
+             DataOutputStream dos = new DataOutputStream(os)) {
+
+            while (true) {
+                String commandMessage = dis.readUTF();
+                System.out.println("ClientHandler: message=" + commandMessage);
+                if (commandMessage.equals("exit")) {
+                    System.exit(1);
+                } else if (commandMessage.equals("get-cookie")) {
+                    String randomCookie = "";
+                    try {
+                        randomCookie = getRandomCookie();
+                    } catch (NoCookieFoundException e) {
+                        System.err.println("ClientHandler: NoCookieFoundException: " + e.getMessage());
                     }
+                    dos.writeUTF("cookie-text_" + randomCookie);
+                } else {
+                    dos.writeUTF("Invalid command");
                 }
             }
+        } catch (IOException e) {
+            System.err.println("ClientHandler: IOException: " + e.getMessage());
         }
     }
 }
